635bab1061f3459d983a7c1de15607c39294b54f
Ben Corcoran <benjamin.corcoran@gmail.com>
2021-01-12 01:17:19 +0100

    Tidy up bucket

--- daily_backup.sh
+++
@@ -1,5 +0,0 @@
-tar -czvf $(date +"%Y-%m-%d")_bucket_backup.tar.gz ./*.json ./*.py ./logs/$(date +"%Y-%m-%d")-bucket.log
-
-aws s3 cp *_bucket_backup.tar.gz s3://facebucket-backup/
-
-rm *_bucket_backup.tar.gz
