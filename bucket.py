d91b7ebbec4e817e642ac84b346f55136d9f83e7
Ben Corcoran <benjamin.corcoran@gmail.com>
2021-02-28 19:46:17 +0000

    update to new name

--- bucket.py
+++ bucket.py
@@ -555,7 +555,7 @@ class Bucket(fbchat.Client):
 
         if thread_type == ThreadType.GROUP:
             ALLUSERS = [user for uid, user in self.fetchUserInfo(
-                *self.fetchGroupInfo(thread_id)[thread_id].participants).items() if user.first_name != 'Bot']
+                *self.fetchGroupInfo(thread_id)[thread_id].participants).items() if user.first_name != 'Bucket']
         else:
             ALLUSERS = [USER]
 
