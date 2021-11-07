2fdfcc6a1b29083b3f8c6fcaa33d4ca54c11bf57
Ben Corcoran <benjamin.corcoran@gmail.com>
2021-11-07 08:46:51 +0200

    Add rebooting cron

--- facebucket/__main__.py
+++ facebucket/__main__.py
@@ -6,8 +6,4 @@ if __name__=='__main__':
     session = get_session()
     bucket = Bucket(session)
     print('Bucket is running...')
-    while True:
-        try:
-            bucket.run()
-        except Exception as e:
-            print(e)
+    bucket.run()
